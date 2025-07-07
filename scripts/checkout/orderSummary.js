9920bfd3523e1743eb8cd902332cff7d2f2229a6
kAtharva4 <163155381+kAtharva4@users.noreply.github.com>
2025-07-07 20:24:49 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,11 +3,8 @@ import {products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import{deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
 
 export function renderOrderSummary(){
   let cartSmmaryHTML = '';
@@ -123,6 +120,8 @@ export function renderOrderSummary(){
         `.js-cart-item-container-${productId}`
       );
       container.remove(); 
+
+      renderPaymentSummary();
     });
   }) ;
 
@@ -131,6 +130,7 @@ export function renderOrderSummary(){
       const {productId, deliveryOptionId} = element.dataset;
       updateDeliveryOption(productId,deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }  
